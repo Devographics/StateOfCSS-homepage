3936564c09cbb1960e7d50ed7c739d8974571a55
SachaG <358832+SachaG@users.noreply.github.com>
2020-10-08 09:11:50 +0900

    Fix issues url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => (
     <div className="Block Block--Footer Footer">
             <p>
-                Hand-coded in Kyoto, Japan.{' '}
-                <a href="https://github.com/StateOfJS/StateOfJS">Leave an issue</a>
+                Hand-coded in Japan.{' '}
+                <a href="https://github.com/StateOfJS/StateOfCSS-homepage">Leave an issue</a>
                 &nbsp;or&nbsp;
                 <a href="mailto:hello@stateofjs.com">Get in touch</a>.
             </p>
