6c7b0bd6fadda229f716ec4f86adf879628a273b
SachaG <358832+SachaG@users.noreply.github.com>
2020-10-19 12:01:23 +0900

    open survey

--- src/components/TakeSurvey.js
+++ src/components/TakeSurvey.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-const surveyUrl = 'https://survey.stateofjs.com/survey/state-of-css/2020';
+const surveyUrl = 'https://app.stateofjs.com/survey/state-of-css/2020';
 
 export default () => (
   <div className="TakeSurvey">
-    <span className="button large-button button-disabled">
+    {/* <span className="button large-button button-disabled">
       Take Survey (Coming Soon!)
-    </span>
-    {/* <a className="button large-button" href={surveyUrl}>
-      Take Survey (Coming Soon!)
-    </a> */}
+    </span> */}
+    <a className="button large-button" href={surveyUrl}>
+      Take Survey
+    </a>
   </div>
 )
