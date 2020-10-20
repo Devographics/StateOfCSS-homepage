0d8a2b4209d13f32f2e597210e64b0f46c56dd0a
SachaG <358832+SachaG@users.noreply.github.com>
2020-10-20 10:30:23 +0900

    track source and referrer and forward them to survey app

--- src/components/TakeSurvey.js
+++ src/components/TakeSurvey.js
@@ -1,14 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import qs from "qs"
 
-const surveyUrl = 'https://survey.stateofjs.com/survey/state-of-css/2020';
+const surveyUrl = "https://survey.stateofjs.com/survey/state-of-css/2020"
 
-export default () => (
-  <div className="TakeSurvey">
-    {/* <span className="button large-button button-disabled">
+export default () => {
+  let suffix = ''
+  const [source, setSource] = useState()
+  const [referrer, setReferrer] = useState()
+
+  useEffect(() => {
+    const q = qs.parse(window.location.href.split('?')[1])
+    setSource(q.source)
+    if (document.referrer) {
+      setReferrer(document.referrer)
+    }
+  })
+
+  if (source || referrer) {
+    suffix = '?'
+    if (source) {
+      suffix += `source=${source}&`
+    }
+    if (referrer) {
+      suffix += `referrer=${referrer}&`
+    }
+  }
+
+  return (
+    <div className="TakeSurvey">
+      {/* <span className="button large-button button-disabled">
       Take Survey (Coming Soon!)
     </span> */}
-    <a className="button large-button" href={surveyUrl}>
-      Take Survey
-    </a>
-  </div>
-)
+      <a className="button large-button" href={surveyUrl + suffix}>
+        Take Survey
+      </a>
+    </div>
+  )
+}
