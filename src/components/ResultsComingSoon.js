cdf9eaf9a7137014f46a76aeebad2d93f40685ce
Sacha Greif <SachaG@users.noreply.github.com>
2021-11-03 07:18:18 +0900

    close survey

--- src/components/ResultsComingSoon.js
+++ src/components/ResultsComingSoon.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ResultsComingSoon = () => (
     <div className="Block Block--ComingSoon ComingSoon introtext">
-        <div className="button button--comingsoon">Survey Over!</div>
+        <h3>Survey Over!</h3>
         <p>We’ve collected data from over 8,000 developers, and we’re now processing the data. Leave us your email below if you’d like to know as soon as the results are published.</p>
     </div>
 )
